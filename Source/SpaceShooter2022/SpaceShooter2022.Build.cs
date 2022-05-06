92a17d1e5f537cf0bad70a044227448ac6fa39dc
michael.stlouis <michael.stlouis@mymail.champlain.edu>
2022-05-05 23:04:53 -0400

    build.cs change

--- Source/SpaceShooter2022/SpaceShooter2022.Build.cs
+++ Source/SpaceShooter2022/SpaceShooter2022.Build.cs
@@ -8,6 +8,7 @@ public class SpaceShooter2022 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Slate", "SlateCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG"});
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 	}
 }
